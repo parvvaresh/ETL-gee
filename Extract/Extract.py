429be329c1304d22997175eb91f393bf38f37f78
parvvaresh <parvvaresh@gmail.com>
2024-06-20 17:29:35 +0330

    fix output type : change form dict to list

---
+++ Extract/Extract.py
@@ -0,0 +1,26 @@
+from _extract_s1 import extract_Sentinel1
+from _extract_s2 import extract_Sentinel2
+import ee
+
+
+def Extract(aoi: ee.Geometry,
+         start_date: str,
+         end_date: str,
+         cloudy_pixel : int) -> dict:
+
+  sentinel1 = extract_Sentinel1(
+  aoi ,
+  start_date,
+  end_date)
+
+  sentinel2 = extract_Sentinel1(
+  aoi ,
+  start_date,
+  end_date,
+  cloudy_pixel)
+
+
+  return {
+    "sentinel1" : sentinel1,
+    "sentinel2" : sentinel2
+  }
