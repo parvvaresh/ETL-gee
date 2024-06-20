02431894f98a4e9970d7654d3273d317778a2bb2
parvvaresh <parvvaresh@gmail.com>
2024-06-20 17:36:15 +0330

    add ETL.py : add extract | transform | load and merge them

---
+++ ELT.py
@@ -0,0 +1,41 @@
+import ee
+
+from Extract.Extract import Extract
+from Transform.Transform import Transform
+from Load.Load import Load
+
+def gee(aoi : ee.Geometry,
+        table_clip : ee.Geometry,
+        cloudy_pixel : int,
+        interval: int,
+        start_date: str,
+        end_date: str,
+        name_file: str,
+        name_folder: str):
+
+
+  sentinel1 , sentinel2 = Extract(aoi,
+         start_date,
+         end_date,
+         cloudy_pixel)
+
+  print("Extract s1 and s2")
+
+
+  reduced_image = Transform(aoi,
+            table_clip,
+            sentinel1,
+            sentinel2,
+            interval,
+            start_date,
+            end_date)
+  print("transforme s1 and s2")
+
+
+
+  Load(reduced_image,
+      name_file,
+      name_folder)
+
+  print("load data on path")
+
---
+++ Load/Load.py
@@ -0,0 +1,11 @@
+from save_csv import save_csv
+
+
+def Load(reduced_image,
+          name_file: str,
+          name_folder: str):
+
+
+  save_csv(reduced_image,
+          name_file,
+          name_folder)
