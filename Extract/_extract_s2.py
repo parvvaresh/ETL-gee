5a39d70e21397f0963a4f7042353fef58930aaa3
parvvaresh <parvvaresh@gmail.com>
2024-06-29 22:41:39 +0330

    better in eara have snow and cloude by add mask_S2_clouds founctions

--- Extract/_extract_s2.py
+++ Extract/_extract_s2.py
@@ -1,43 +1,35 @@
 import ee
+import time
 
 
 def mask_s2_clouds(image):
-
-  """Masks clouds in a Sentinel-2 image using the QA band.
-
-  Args:
-      image (ee.Image): A Sentinel-2 image.
-
-  Returns:
-      ee.Image: A cloud-masked Sentinel-2 image.
+  """M
+    asks clouds in a Sentinel-2 image using the QA band.
   """
+
   qa = image.select('QA60')
 
-  # Bits 10 and 11 are clouds and cirrus, respectively.
   cloud_bit_mask = 1 << 10
   cirrus_bit_mask = 1 << 11
 
-  # Both flags should be set to zero, indicating clear conditions.
   mask = (
-      qa.bitwiseAnd(cloud_bit_mask)
-      .eq(0)
-      .And(qa.bitwiseAnd(cirrus_bit_mask).eq(0))
-  )
-
+        qa.bitwiseAnd(cloud_bit_mask).eq(0)
+        .And(qa.bitwiseAnd(cirrus_bit_mask).eq(0))
+    )
   return image.updateMask(mask).divide(10000)
 
 
 
-def extract_Sentinel2(aoi: ee.Geometry,
-                    start_date: str,
-                    end_date: str,
-                    cloudy_pixel: int) -> None:
 
+def extract_Sentinel2(aoi: ee.Geometry,
+                      start_date: str,
+                      end_date: str,
+                      cloudy_pixel: int) -> ee.ImageCollection:
   sentinel2 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED') \
-            .filter(ee.Filter.lessThanOrEquals('CLOUDY_PIXEL_PERCENTAGE', cloudy_pixel)) \
-            .filterBounds(aoi) \
-            .filterDate(start_date, end_date) \
-            .map(mask_s2_clouds)
+                .filter(ee.Filter.lessThanOrEquals('CLOUDY_PIXEL_PERCENTAGE', cloudy_pixel)) \
+                .filterBounds(aoi) \
+                .filterDate(start_date, end_date) \
+                .map(mask_s2_clouds)
 
   if sentinel2.size().getInfo() == 0:
     raise ValueError("Sentinel-2 collection is empty. Check your date range or AOI.")
