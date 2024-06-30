bdde962caadc8a2bea97fab167040ae034b17036
parvvaresh <parvvaresh@gmail.com>
2024-06-30 13:16:33 +0330

    delete s2 from code | clean code

--- Extract/Extract.py
+++ Extract/Extract.py
@@ -1,14 +1,11 @@
 from ._extract_s1 import extract_Sentinel1
-from ._extract_s2 import extract_Sentinel2
 import ee
 
 
 def Extract(aoi: ee.Geometry,
             start_date: str,
-            end_date: str,
-            cloudy_pixel: int) -> list:
+            end_date: str) -> list:
 
 
    sentinel1 = extract_Sentinel1(aoi, start_date, end_date)
-   sentinel2 = extract_Sentinel2(aoi, start_date, end_date, cloudy_pixel)
-   return [sentinel1, sentinel2]
+   return sentinel1
