cda9bfddeae91030c8e38b5c1fd3b0d37f0c3529
parvvaresh <parvvaresh@gmail.com>
2024-07-01 12:41:16 +0330

    fix variables

--- ETL.py
+++ ETL.py
@@ -1,11 +1,10 @@
+import ee
 
-from Extract._extract_s2 import Extract
+from Extract._extract_s2 import extract_Sentinel2
 from Extract.Extract import Extract
 
 from Transform.Transform import Transform
 from Load.Load import Load
-
-import ee
 
 def gee(aoi : ee.Geometry,
         start_date: str,
@@ -16,28 +15,27 @@ def gee(aoi : ee.Geometry,
         name_folder: str):
 
 
-  sentinel1  = Extract(aoi,
+    sentinel1  = Extract(aoi,
          start_date,
          end_date,
          cloudy_pixel)
 
-  print("Extract s1 and s2")
+    print("Extracted Sentinel-1 and Sentinel-2 data.")
 
 
-  reduced_image = Transform(aoi,
-            table_clip,
+    reduced_image = Transform(aoi,
             sentinel1,
-            sentinel2,
+            extract_Sentinel2,
             interval,
             start_date,
-            end_date)
-  print("transforme s1 and s2")
+            end_date,
+            cloudy_pixel)
+    print("Transformed Sentinel-1 and Sentinel-2 data.")
 
 
 
-  Load(reduced_image,
+    Load(reduced_image,
       name_file,
       name_folder)
 
-  print("load data on path")
-
+    print("Data saved to path.")
