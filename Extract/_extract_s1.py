cf30bc06789b6fbfd6b70fa094792148b4b652b2
parvvaresh <parvvaresh@gmail.com>
2024-11-24 14:12:31 +0330

    fix bugs :: change stalate

--- Extract/_extract_s1.py
+++ Extract/_extract_s1.py
@@ -1,15 +1,16 @@
 import ee
 
 
-def extract_Sentinel1(aoi: ee.Geometry,
-                      start_date: str,
-                      end_date: str) -> ee.ImageCollection:
-
-  sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD') \
-                    .filterDate(start_date, end_date) \
-                    .filterBounds(aoi)
-
-  if sentinel1.size().getInfo() == 0:
-    raise ValueError("Sentinel-1 collection is empty. Check your date range or AOI.")
-
-  return sentinel1
+def extract_Sentinel1(aoi : ee.Geometry, start_date : str, end_date : str) -> ee.ImageCollection:
+    """
+      Extract Sentinel-1 radar data.
+    """
+    sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD') \
+                .filterDate(start_date, end_date) \
+                .filterBounds(aoi) \
+                .select(['VV', 'VH'])
+    
+    if sentinel1.size().getInfo() == 0:
+        raise ValueError("Sentinel-1 collection is empty. Check your date range or AOI.")
+    
+    return sentinel1
